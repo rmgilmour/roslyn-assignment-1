d63778c5c1dca256e8628d16db028ea0f189cb3a
rmgilmour <rmgilmour@users.noreply.github.com>
2022-12-19 17:57:03 -0500

    Update Assignment1Application.java

--- src/com/websitenamehereforpackage/Assignment1Application.java
+++ src/com/websitenamehereforpackage/Assignment1Application.java
@@ -1,5 +1,12 @@
 package com.websitenamehereforpackage;
 
 public class Assignment1Application {
+	
+	public static void main(String[] args) {
+		
+		// generate a random number
+		
+		//and collect user input
+	}
 
 }
